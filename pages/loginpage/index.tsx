cb1d34ff260885fc537b05055a979ee179d17976
Oscar Ramos <64324454+lacroix021@users.noreply.github.com>
2022-01-31 18:05:56 -0500

    updated

--- pages/loginpage/index.tsx
+++ pages/loginpage/index.tsx
@@ -156,7 +156,7 @@ export default function LoginPage(ssr = true) {
                                                 <Field
                                                     type="password"
                                                     id="password"
-                                                    name="password"
+                                                    name="Retype-password"
                                                     placeholder="confirm password"
                                                     className={styles.input}
                                                 />
